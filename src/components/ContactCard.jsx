3f55dccef90850802964171e27108581b4888d1c
Ian Garvey <ianarthurgarvey@gmail.com>
2025-06-14 01:00:52 +0000

    adds some styling for the contact card component

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -17,15 +17,32 @@ export const ContactCard = ({name, email, phone, address, id}) => {
         }
     }
     return (
-        <div>
-            <h1>hello {name}</h1>
-            <p>{email}</p>
-            <p>{phone}</p>
-            <p>{address}</p> 
-            <Link to={`/edit-contact/${id}`}>
-                <button className="btn btn-primary">Edit</button>
-            </Link>
-            <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+        <div className="contact-container d-flex justify-content-between" style={{border: "1px solid #74C0FC", borderRadius: "10px", padding: "20px", marginBottom: "10px"}}>
+            
+            <div className="image-container">
+                <i className="fa-solid fa-user" style={{ fontSize: "100px", color: "#74C0FC", backgroundColor: "", borderRadius: "50%"}}></i>
+            </div>
+
+            <div className="contact-details">
+                <h1>{name}</h1>
+                    <div className="email d-flex align-items-center">
+                        <i className="fa-solid fa-envelope p" style={{color: "#74C0FC"}}></i><p>{email}</p>
+                    </div>
+                <div className="phone">
+                    <i className="fa-solid fa-phone" style={{color: "#74C0FC"}}></i><p>{phone}</p>
+                </div>
+                <div className="address">
+                    <i className="fa-solid fa-location-dot" style={{color: "#74C0FC"}}></i><p>{address}</p> 
+                </div>
+            </div>
+
+            <div className="contact-actions">
+                <Link to={`/edit-contact/${id}`}>
+                    <button className="btn btn-primary">Edit</button>
+                </Link>
+                <button className="btn btn-danger" onClick={handleDelete}>Delete</button>
+            </div>
+
         </div>
         
     )
